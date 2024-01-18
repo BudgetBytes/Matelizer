0547220cc073ad688fe9dcc671e149188cf73dbd
pierpaolo.feltrin <holytuta@tutanota.com>
2024-01-18 10:37:29 +0100

    Fixed float overflow bug

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
-#define MAX_POINTS 100000
+#define MAX_POINTS 50000
 
 int main(int argc, char **argv) 
 {
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-    float theta0 = 0.01f * atof(thetaArg);
+    double theta0 = 0.01l * atof(thetaArg);
 
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pi Visualization");
@@ -33,10 +33,10 @@ int main(int argc, char **argv)
     Camera2D cam = {0};
     cam.zoom = 1;
     
-    float theta1 = theta0;
-    float theta = 0.0f;
+    double theta1 = theta0;
+    double theta = 0.0l;
 
-    float _Complex outerPath[MAX_POINTS];
+    double _Complex outerPath[MAX_POINTS];
     int pathIndex = 0;
 
     while (!WindowShouldClose()) {
@@ -44,27 +44,27 @@ int main(int argc, char **argv)
         // translate based on left click
         if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
             Vector2 delta = GetMouseDelta();
-            delta = Vector2Scale(delta, -1.0f / cam.zoom);
+            delta = Vector2Scale(delta, -1.0l / cam.zoom);
             cam.target = Vector2Add(cam.target, delta);
         }
 
         // zoom based on wheel
-        float wheel = GetMouseWheelMove();
+        double wheel = GetMouseWheelMove();
         if (wheel != 0) {
             Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), cam);
             cam.offset = GetMousePosition();
             cam.target = mouseWorldPos;
 
             // zoom
-            cam.zoom += wheel * 1.25f;
-            if (cam.zoom < 1.0f)
-                cam.zoom = 1.0f;
+            cam.zoom += wheel * 1.25l;
+            if (cam.zoom < 1.0l)
+                cam.zoom = 1.0l;
         }
 
         theta += theta1;
 
-        float _Complex innerRod = cexpf(I * theta);
-        float _Complex outerRod = cexpf(I * M_PI * theta);
+        double _Complex innerRod = cexpf(I * theta);
+        double _Complex outerRod = cexpf(I * M_PI * theta);
 
         outerPath[pathIndex] = innerRod + outerRod;
 
@@ -74,13 +74,14 @@ int main(int argc, char **argv)
         }
 
         Vector2 center = {
-            .x = GetScreenWidth() / 2.0f,
-            .y = GetScreenHeight() / 2.0f,
+            .x = GetScreenWidth() / 2.0l,
+            .y = GetScreenHeight() / 2.0l,
         };
 
         if (IsKeyReleased(KEY_N)) {
             pathIndex = 0;
             theta1 += theta0;
+            printf("Theta: %lf\n", theta1);
         }
 
         if (IsKeyReleased(KEY_B)) {
@@ -104,10 +105,11 @@ int main(int argc, char **argv)
             EndMode2D();
 
             DrawText("Press ESC to exit", 10, 10, 20, WHITE);
-            DrawText("N -> Next animmation", 10, 40, 20, WHITE);
-            DrawText("B -> Previous animmation", 10, 70, 20, WHITE);
+            DrawText("N -> Next animation", 10, 40, 20, WHITE);
+            DrawText("B -> Previous animation", 10, 70, 20, WHITE);
             DrawText(TextFormat("Frame: %i", pathIndex), 10, 100, 20, WHITE);
             DrawText(TextFormat("/%i", MAX_POINTS), 150, 100, 20, WHITE);
+            DrawText(TextFormat("Theta: %lf", theta1), 10, 130, 20, WHITE);
         }
         EndDrawing();
     }
